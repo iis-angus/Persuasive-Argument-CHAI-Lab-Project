e7088df810e765f07406bd9a22c6236043c7b2ec
Angus Chen <angusc3@illinois.edu>
2025-10-16 14:08:31 -0500

    features

--- features.py
+++ features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 
 explain_path = 'results/explain_then_predict_results.csv'
 predict_path = 'results/predict_then_explain_results.csv'
@@ -35,9 +36,8 @@ def load_concreteness_lexicon():
     
     try:
         df = pd.read_excel(lexicon_path)
-        
+        df['Conc.M'] /= 5 #normalize to range [0, 1]
         concreteness_dict = dict(zip(df['Word'].str.lower(), df['Conc.M']))
-        concreteness_dict['Conc.M'] /= 5 #normalize to range [0, 1]
         return concreteness_dict
     
     except Exception as e:
@@ -79,7 +79,7 @@ def average_valence_score(text):
     valence_scores = []
     
     for word in words:
-        if word in valence_lexicon:
+        if word in valence_lexicon and abs(valence_lexicon[word]) > 0.7:
             valence_scores.append(valence_lexicon[word])
     
     if valence_scores:
@@ -109,7 +109,7 @@ def average_arousal_score(text):
     arousal_scores = []
     
     for word in words:
-        if word in arousal_lexicon:
+        if word in arousal_lexicon and abs(arousal_lexicon[word]) > 0.7:
             arousal_scores.append(arousal_lexicon[word])
     
     if arousal_scores:
@@ -139,7 +139,7 @@ def average_dominance_score(text):
     dominance_scores = []
     
     for word in words:
-        if word in dominance_lexicon:
+        if word in dominance_lexicon and abs(dominance_lexicon[word]) > 0.7:
             dominance_scores.append(dominance_lexicon[word])
     
     if dominance_scores:
@@ -170,7 +170,7 @@ def average_concreteness_score(text):
     concreteness_scores = []
     
     for word in words:
-        if word in concreteness_lexicon:
+        if word in concreteness_lexicon and abs(concreteness_lexicon[word]) > 0.7:
             concreteness_scores.append(concreteness_lexicon[word])
     
     if concreteness_scores:
@@ -184,6 +184,12 @@ def word_count(text):
         return 0
     return len(str(text).split())
 
+#Returns number of links/url in a text
+def count_links(text):
+    if pd.isna(text):
+        return 0
+    return len(re.findall(r'https?://\S+', str(text)))
+ 
 # ----------- MAIN FUNCTION ----------- #
 #Processes features (MAIN WRAPPER)
 def process_features(csv_path):
@@ -191,17 +197,54 @@ def process_features(csv_path):
     
     data = []
     for index, row in df.iterrows():
+        r1_word_count = word_count(row['response_1'])
+        r2_word_count = word_count(row['response_2'])
+        r1_valence = average_valence_score(row['response_1'])
+        r2_valence = average_valence_score(row['response_2'])
+        r1_arousal = average_arousal_score(row['response_1'])
+        r2_arousal = average_arousal_score(row['response_2'])
+        r1_dominance = average_dominance_score(row['response_1'])
+        r2_dominance = average_dominance_score(row['response_2'])
+        r1_concreteness = average_concreteness_score(row['response_1'])
+        r2_concreteness = average_concreteness_score(row['response_2'])
+        r1_links = count_links(row['response_1'])
+        r2_links = count_links(row['response_2'])
+
+        # Determine sign based on prediction
+        if row["prediction"] == 1:
+            prediction_word_count_diff = r1_word_count - r2_word_count
+            prediction_valence_diff = r1_valence - r2_valence
+            prediction_arousal_diff = r1_arousal - r2_arousal
+            prediction_dominance_diff = r1_dominance - r2_dominance
+            prediction_concrete_diff = r1_concreteness - r2_concreteness
+            prediction_link_diff = r1_links - r2_links
+        else:
+            prediction_word_count_diff = r2_word_count - r1_word_count
+            prediction_valence_diff = r2_valence - r1_valence
+            prediction_arousal_diff = r2_arousal - r1_arousal
+            prediction_dominance_diff = r2_dominance - r1_dominance
+            prediction_concrete_diff = r2_concreteness - r1_concreteness
+            prediction_link_diff = r2_links - r1_links
+
         row_data = {
-            'response_1_word_count': word_count(row['response_1']),
-            'response_2_word_count': word_count(row['response_2']),
-            'response_1_valence': average_valence_score(row['response_1']),
-            'response_2_valence': average_valence_score(row['response_2']),
-            'response_1_arousal': average_arousal_score(row['response_1']),
-            'response_2_arousal': average_arousal_score(row['response_2']),
-            'response_1_dominance': average_dominance_score(row['response_1']),
-            'response_2_dominance': average_dominance_score(row['response_2']),
-            'response_1_concreteness': average_concreteness_score(row['response_1']),
-            'response_2_concreteness': average_concreteness_score(row['response_2'])
+            'response_1_word_count': r1_word_count,
+            'response_2_word_count': r2_word_count,
+            'word_count_prediction_difference': prediction_word_count_diff,
+            'response_1_valence': r1_valence,
+            'response_2_valence': r2_valence,
+            'valence_prediction_difference': prediction_valence_diff,
+            'response_1_arousal': r1_arousal,
+            'response_2_arousal': r2_arousal,
+            'arousal_prediction_difference': prediction_arousal_diff,
+            'response_1_dominance': r1_dominance,
+            'response_2_dominance': r2_dominance,
+            'dominance_prediction_difference': prediction_dominance_diff,
+            'response_1_concreteness': r1_concreteness,
+            'response_2_concreteness': r2_concreteness,
+            'concreteness_prediction_difference': prediction_concrete_diff,
+            'response_1_link_count': r1_links,
+            'response_2_link_count': r2_links,
+            'link_count_prediction_difference': prediction_link_diff
         }
         
         data.append(row_data)
@@ -213,4 +256,3 @@ if __name__ == '__main__':
     concreteness_lexicon = load_concreteness_lexicon()
     process_features(explain_path).to_csv('results/explain_features.csv', index=False)
     process_features(predict_path).to_csv('results/predict_features.csv', index=False)
-
